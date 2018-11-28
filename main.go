0328af00662145d265ab9403dd6105711fbe1d99
Steve Birtles <sbirtles@farnborough.ac.uk>
2018-11-28 16:25:38 +0000

    Bomberman!

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
-	"github.com/faiface/pixel/pixelgl"
 	"github.com/faiface/pixel"
-	"os"
-	"image"
-	"time"
+	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
+	"image"
 	_ "image/png"
+	"net/http"
+	"os"
 	"strings"
+	"time"
 )
 
 const screenWidth = 1024
@@ -19,12 +19,14 @@ const screenHeight = 768
 var (
 	frames            = 0
 	second            = time.Tick(time.Second)
-	windowTitlePrefix	= "Game client with an API"
+	quarter           = time.Tick(time.Second / 10)
+	windowTitlePrefix = "Game client with an API"
 	win               *pixelgl.Window
-	sprite            *pixel.Sprite
+	bombermanSprite   [4][4][4]*pixel.Sprite
+	bombermanFrame    = 0
 	spriteBatch       *pixel.Batch
-	x = float64(screenWidth/2)
-	y = float64(screenHeight/2)
+	x                 = float64(screenWidth / 2)
+	y                 = float64(screenHeight / 2)
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -38,15 +40,19 @@ func moveHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	pathBits := strings.Split( r.URL.Path, "/")
+	pathBits := strings.Split(r.URL.Path, "/")
 	direction := pathBits[len(pathBits)-1]
 	fmt.Println("/direction/", direction)
 
 	switch direction {
-		case "up": y += 25
-		case "down": y -= 25
-		case "left": x -= 25
-		case "right": x += 25
+	case "up":
+		y += 25
+	case "down":
+		y -= 25
+	case "left":
+		x -= 25
+	case "right":
+		x += 25
 	}
 
 }
@@ -78,14 +84,21 @@ func client() {
 		panic(initError)
 	}
 
-	spriteImage, initError := loadImageFile("sonic.png")
+	spriteImage, initError := loadImageFile("bomberman.png")
 	if initError != nil {
 		panic(initError)
 	}
 
 	spritePic := pixel.PictureDataFromImage(spriteImage)
 
-	sprite = pixel.NewSprite(spritePic, spritePic.Bounds())
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			for k := 0; k < 3; k++ {
+				bombermanSprite[i][j][k] = pixel.NewSprite(spritePic, pixel.R(float64(k+j*4)*20, float64(120-30*(i+1)), float64(k+j*4+1)*20, float64(120-30*i)))
+			}
+			bombermanSprite[i][j][3] = bombermanSprite[i][j][1]
+		}
+	}
 
 	spriteBatch = pixel.NewBatch(&pixel.TrianglesData{}, spritePic)
 
@@ -93,9 +106,18 @@ func client() {
 
 		spriteBatch.Clear()
 
-		matrix := pixel.IM.Rotated(pixel.ZV, 0).Scaled(pixel.ZV, 0.2).Moved(pixel.Vec{X: x, Y: y})
+		matrix := pixel.IM.Rotated(pixel.ZV, 0).Scaled(pixel.ZV, 4).Moved(pixel.Vec{X: x, Y: y})
 
-		sprite.Draw(spriteBatch, matrix)
+		select {
+		case <-quarter:
+			bombermanFrame--
+			if bombermanFrame < 0 {
+				bombermanFrame = 3
+			}
+		default:
+		}
+
+		bombermanSprite[0][0][bombermanFrame].Draw(spriteBatch, matrix)
 
 		win.Clear(colornames.Black)
 
