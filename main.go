7deeeecb62cc2f84d78d24a5ca10623f0696fd30
Steve Birtles <sbirtles@farnborough.ac.uk>
2018-11-28 22:43:47 +0000

    4 players random walk.

--- main.go
+++ main.go
@@ -11,22 +11,28 @@ import (
 	"os"
 	"strings"
 	"time"
+	"math"
+	"math/rand"
 )
 
 const screenWidth = 1024
 const screenHeight = 768
 
+type Player struct {
+	lastP, lastQ, targetP, targetQ int
+	x, y, progress                 float64
+	frame, direction               int
+}
+
 var (
-	frames            = 0
-	second            = time.Tick(time.Second)
-	quarter           = time.Tick(time.Second / 10)
-	windowTitlePrefix = "Game client with an API"
-	win               *pixelgl.Window
-	bombermanSprite   [4][4][4]*pixel.Sprite
-	bombermanFrame    = 0
-	spriteBatch       *pixel.Batch
-	x                 = float64(screenWidth / 2)
-	y                 = float64(screenHeight / 2)
+	frames = 0
+	second = time.Tick(time.Second)
+	tick   = time.Tick(time.Second / 16)
+	title  = "Game client with an API"
+	win    *pixelgl.Window
+	sprite [4][4][4]*pixel.Sprite
+	batch  *pixel.Batch
+	player [4]Player
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -46,13 +52,13 @@ func moveHandler(w http.ResponseWriter, r *http.Request) {
 
 	switch direction {
 	case "up":
-		y += 25
+		player[0].y += 25
 	case "down":
-		y -= 25
+		player[0].y -= 25
 	case "left":
-		x -= 25
+		player[0].x -= 25
 	case "right":
-		x += 25
+		player[0].x += 25
 	}
 
 }
@@ -94,42 +100,92 @@ func client() {
 	for i := 0; i < 4; i++ {
 		for j := 0; j < 4; j++ {
 			for k := 0; k < 3; k++ {
-				bombermanSprite[i][j][k] = pixel.NewSprite(spritePic, pixel.R(float64(k+j*4)*20, float64(120-30*(i+1)), float64(k+j*4+1)*20, float64(120-30*i)))
+				sprite[i][j][k] = pixel.NewSprite(spritePic,
+					pixel.R(float64(k+j*3)*20, float64(120-30*(i+1)), float64(k+j*3+1)*20, float64(120-30*i)))
 			}
-			bombermanSprite[i][j][3] = bombermanSprite[i][j][1]
+			sprite[i][j][3] = sprite[i][j][1]
 		}
 	}
 
-	spriteBatch = pixel.NewBatch(&pixel.TrianglesData{}, spritePic)
+
+	for i := range player {
+		player[i].lastP = int((i % 2) * 4 + 2)
+		player[i].lastQ = int(math.Floor(float64(i / 2)) * 4 + 2)
+		player[i].targetP = player[i].lastP
+		player[i].targetQ = player[i].lastQ
+		player[i].direction = i
+		player[i].progress = 1
+	}
+
+	batch = pixel.NewBatch(&pixel.TrianglesData{}, spritePic)
 
 	for !win.Closed() {
 
-		spriteBatch.Clear()
-
-		matrix := pixel.IM.Rotated(pixel.ZV, 0).Scaled(pixel.ZV, 4).Moved(pixel.Vec{X: x, Y: y})
+		batch.Clear()
 
 		select {
-		case <-quarter:
-			bombermanFrame--
-			if bombermanFrame < 0 {
-				bombermanFrame = 3
+		case <-tick:
+			for i := range player {
+				if player[i].progress < 1 {
+					player[i].progress += 0.125
+					player[i].frame--
+					if player[i].frame < 0 {
+						player[i].frame = 3
+					}
+				}
+				if player[i].progress >= 1 {
+					player[i].progress = 1
+					player[i].frame = 3
+					player[i].lastP = player[i].targetP
+					player[i].lastQ = player[i].targetQ
+
+					player[i].direction = rand.Intn(4)
+					switch player[i].direction {
+					case 0:
+						if player[i].lastP > 1 {
+							player[i].targetP--
+							player[i].progress = 0
+						}
+					case 1:
+						if player[i].lastQ < screenHeight/80-1 {
+							player[i].targetQ++
+							player[i].progress = 0
+						}
+					case 2:
+						if player[i].lastQ > 1 {
+							player[i].targetQ--
+							player[i].progress = 0
+						}
+					case 3:
+						if player[i].lastP < screenWidth/80 {
+							player[i].targetP++
+							player[i].progress = 0
+						}
+					}
+
+				}
+				player[i].x = 80 * (float64(player[i].lastP) + float64(player[i].targetP - player[i].lastP) * player[i].progress)
+				player[i].y = screenHeight - 80 * (float64(player[i].lastQ) + float64(player[i].targetQ - player[i].lastQ) * player[i].progress)
 			}
 		default:
 		}
 
-		bombermanSprite[0][0][bombermanFrame].Draw(spriteBatch, matrix)
+		for i, p := range player {
+			matrix := pixel.IM.Rotated(pixel.ZV, 0).Scaled(pixel.ZV, 4).Moved(pixel.Vec{X: p.x, Y: p.y})
+			sprite[i][p.direction][p.frame].Draw(batch, matrix)
+		}
 
 		win.Clear(colornames.Black)
 
 		win.SetComposeMethod(pixel.ComposeOver)
-		spriteBatch.Draw(win)
+		batch.Draw(win)
 
 		win.Update()
 
 		frames++
 		select {
 		case <-second:
-			win.SetTitle(fmt.Sprintf("%s | FPS: %d", windowTitlePrefix, frames))
+			win.SetTitle(fmt.Sprintf("%s | FPS: %d", title, frames))
 			frames = 0
 		default:
 		}
